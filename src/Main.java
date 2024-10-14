9b61981b2dc61d03278ec4304eb9ef9021231db3
nyadiore <robinn6@mail.uc.edu>
2024-10-13 22:22:39 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+            // Loop that counts up by 1 from 0 to 30
+
+            for (int i = 0; i <= 30; i++) {
+
+                System.out.print(i + " "); // Print each number followed by a space
+
+            }
+
+        }
+
+    }
